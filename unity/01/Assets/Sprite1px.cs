d258e4b64008f76ee3aaf8cc6195d4165aeab131
yoggy <yoggy0@gmail.com>
2015-06-10 16:26:10 +0900

    added unity_01

---
+++ unity/01/Assets/Sprite1px.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sprite1px : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+
+	// spritesの内容はUnityのInspectorから設定すること
+	public Sprite[] sprites = new Sprite[4];
+
+
+	int idx = 0;
+
+	void Start ()
+	{
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+	void Update () 
+	{
+		int idx = (int)(Time.fixedTime / 0.25) % 4;
+		spriteRenderer.sprite = sprites[idx];
+	}
+}
